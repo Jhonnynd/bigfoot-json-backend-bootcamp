c6471be2800dc1e94d1bf203fbaa879fc31f233d
Jhonnynd <jhonnynd@gmail.com>
2023-07-20 22:28:08 +0800

    finished base project json

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const { getSightings } = require("./utils.js");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT;
-const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  res.json(
+    sightings.filter(
+      (sighting) => sighting.REPORT_NUMBER === req.params.sightingIndex
+    )
+  );
 });
 
 app.listen(PORT, () => {
